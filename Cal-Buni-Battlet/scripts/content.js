c4c25b031210d423980da3f880a218da9b9a352e
Lữ Khách Độc Hành <7509414+ganhip@users.noreply.github.com>
2021-09-30 08:56:21 +0700

    Add files via upload

--- Cal-Buni-Battlet/scripts/content.js
+++ Cal-Buni-Battlet/scripts/content.js
@@ -1,5 +1,6 @@
 
 var trainPower = 0,
+    train_elm = '',
     buni_element = '',
     buni_star = 1,
     // bunicornPowerMultiplier = 1 + SUM(attribute * factor) / attributeBaseline
@@ -8,11 +9,15 @@ var trainPower = 0,
     // playerBasePower=trainerPower∗bunicornPowerMultiplier
     playerBasePower = 0,
     attributeBaseline = 500;
-
-var factor_neutral = 1.05,
-    factor_all = 1.05,
-    factor_Duplicate = 1.1,
-    bonus_factor = 1.05;
+// V2: https://forum.buni.finance/t/update-bunicorn-game-version-2/234
+// update 29/09 
+// https://forum.buni.finance/t/bunicorn-game-update-18-00-vnt-ngay-29-9-2021/267?fbclid=IwAR1ZmdmnTfVdE8R15B07N3CoC0ZQxb8W1fzS4i0AX8Eifqd8N7TvBRVArtA
+var factor_Duplicate = 1.15,     // trainer same element with pet. Old val : 1.1
+    factor_neutral = 1.07,      // old val: 1.05
+    bonus_factor = 1.07,        // old val: 1.05
+    factor_all = 1,             // old val 1 
+    elementalBonus = 1
+    ;
 
 $(document).on('click', 'div.enemy-container', function () {
     // console.log('enemy-container clicked');
@@ -22,7 +27,6 @@ $(document).on('click', 'div.enemy-container', function () {
 // $('body').on('DOMSubtreeModified', 'div.list-enemies', function () {
 //     console.log('div.list-enemies changed');
 // });
-
 
 function trainerSelected() {
     var _trainPower = 0;
@@ -41,44 +45,133 @@ function trainerSelected() {
     return _trainPower;
 }
 
+function getTrainerElement() {
+    var t_elm = 'train_elm';
+    var imgTag = $("img.selected-trainner-image");
+    try {
+        var imgLink = imgTag.first().attr('src');
+
+        var imgStr = imgLink.split('/');
+        if (imgStr.length > 0) {
+            imgStr = imgStr[imgStr.length - 1];
+        }
+        // console.log('trainer imgStr:' + imgStr);
+        if (imgStr.indexOf('fire') != -1) {
+            t_elm = 'fire';
+        }
+        if (imgStr.indexOf('air') != -1) {
+            t_elm = 'air';
+        }
+        if (imgStr.indexOf('water') != -1) {
+            t_elm = 'water';
+        }
+        if (imgStr.indexOf('earth') != -1) {
+            t_elm = 'earth';
+        }
+    } catch (error) {
+        console.log('getTrainerElement error:' + error)
+    }
+    // console.log('trainer element:' + t_elm);
+    return t_elm;
+}
+
+function getEnemyElement(src) {
+    var t_elm = 'emy_elm';
+    try {
+        var imgStr = src.split('/');
+        if (imgStr.length > 0) {
+            imgStr = imgStr[imgStr.length - 1];
+        }
+        // console.log('trainer imgStr:' + imgStr);
+        if (imgStr.indexOf('fire') != -1) {
+            t_elm = 'fire';
+        }
+        if (imgStr.indexOf('air') != -1) {
+            t_elm = 'air';
+        }
+        if (imgStr.indexOf('water') != -1) {
+            t_elm = 'water';
+        }
+        if (imgStr.indexOf('earth') != -1) {
+            t_elm = 'earth';
+        }
+    } catch (error) {
+        console.log('getTrainerElement error:' + error)
+    }
+    // console.log('trainer element:' + t_elm);
+    return t_elm;
+}
+
 function getBuniElement() {
-    var _buni_element = '';
+    var b_elm = 'pet_elm';
     var buni_html_elm = $("img.selected-trainner-image");
     // console.log(buni_html_elm.last().attr('src'));
     try {
-        var imglink = buni_html_elm.last().attr('src');
-        var arrStr = imglink.split('/');
-        if (arrStr.length > 0) {
-            arrStr = arrStr[arrStr.length - 1];
+        var imgLink = buni_html_elm.last().attr('src');
+        var imgStr = imgLink.split('/');
+        if (imgStr.length > 0) {
+            imgStr = imgStr[imgStr.length - 1];
         }
-        arrStr = arrStr.split('_');
-        if (arrStr.length >= 3) {
-            buni_star = arrStr[2];
+        imgStr = imgStr.split('_');
+        if (imgStr.length >= 3) {
+            buni_star = imgStr[2];
         }
         // console.log('buni_star ' + buni_star);
-        imglink = imglink.toLowerCase();
-        if (imglink.indexOf('fire') != -1) {
-            _buni_element = 'fire';
+        imgLink = imgLink.toLowerCase();
+        if (imgLink.indexOf('fire') != -1) {
+            b_elm = 'fire';
+        } if (imgLink.indexOf('earth') != -1) {
+            b_elm = 'earth';
         }
-        if (imglink.indexOf('air') != -1) {
-            _buni_element = 'air';
+        if (imgLink.indexOf('air') != -1) {
+            b_elm = 'air';
         }
-        if (imglink.indexOf('water') != -1) {
-            _buni_element = 'water';
+        if (imgLink.indexOf('water') != -1) {
+            b_elm = 'water';
         }
-        if (imglink.indexOf('earth') != -1) {
-            _buni_element = 'earth';
-        }
+
         // console.log('buni_element: ' + _buni_element);
     } catch (error) {
         console.log("getBuniElement error:" + error)
     }
-    return _buni_element;
+    return b_elm;
+}
+
+function calTrainerElementBonus(emy_elm) {
+    var result = 0;
+    if (train_elm == buni_element) {
+        result = elementalBonus + 0.052;
+    } else {
+        result = elementalBonus;
+    }
+    // console.log('playElementBonus 1=' + result);
+    emy_elm = emy_elm.toLowerCase();
+    // console.log(train_elm + '-' + buni_element + '-' + emy_elm);
+    switch (train_elm) {
+        case 'fire':
+            if (emy_elm == 'earth') { result += 0.052; }
+            if (emy_elm == 'water') { result -= 0.052; }
+            break;
+        case 'earth':
+            if (emy_elm == 'air') { result += 0.052; }
+            if (emy_elm == 'fire') { result -= 0.052; }
+            break;
+        case 'air':
+            if (emy_elm == 'water') { result += 0.052; }
+            if (emy_elm == 'earth') { result -= 0.052; }
+            break;
+        case 'water':
+            if (emy_elm == 'fire') { result + 0.052; }
+            if (emy_elm == 'air') { result - 0.052; }
+            break;
+    }
+    return result;
 }
 
 function calculatorWinRate() {
     try {
         trainPower = trainerSelected();
+        train_elm = getTrainerElement();
         var buni_html_elm = $("img.selected-trainner-image");
         if (buni_html_elm.length > 1) {
             buni_element = getBuniElement();
@@ -132,13 +225,6 @@ function calculatorWinRate() {
             // playerBasePower
             playerBasePower = trainPower * bunicornPowerMultiplier;
             // console.log('playerBasePower:' + playerBasePower);
-            // var buni_html_info_left = $("div.info-selected-bunicorn .left-info");   // I not hava more Attribute
-            var playPower_min = playerBasePower * 0.9;
-            playPower_min = Math.round(playPower_min);
-            var playPower_max = playerBasePower * 1.1;
-            playPower_max = Math.round(playPower_max);
-            // console.log('playPower_min: ' + playPower_min);
-            // console.log('playPower_max: ' + playPower_max);
 
             // Get list Enemy
             var list_enemy = $('div.enemy');
@@ -148,7 +234,26 @@ function calculatorWinRate() {
                 $('<div id="info-win-lose-ext" class="elements info-win-lose-ext"></div>').insertBefore('div.list-enemies');
             }
             $(list_enemy).each(function (idx) {
-                // console.log(index + ": " + $(this).html());
+                // console.log(idx + ": " + $(this).html());
+                // Enemy Element
+                var emy_elm_link = $(this).find("div.enemy-img").find('img');
+                emy_elm_link = emy_elm_link.attr('src');
+                // console.log('elm_link: ' + emy_elm_link);
+                emy_elm = getEnemyElement(emy_elm_link);
+                // console.log('getEnemyElement: ' + emy_elm);
+
+                var playElementBonus = calTrainerElementBonus(emy_elm);
+                // console.log('playElementBonus: ' + playElementBonus);
+                var playerBasePower_final = playerBasePower * playElementBonus;
+
+                // var buni_html_info_left = $("div.info-selected-bunicorn .left-info");   // I not hava more Attribute
+                var playPower_min = playerBasePower_final * 0.9;
+                playPower_min = Math.round(playPower_min);
+                var playPower_max = playerBasePower_final * 1.1;
+                playPower_max = Math.round(playPower_max);
+                // console.log('playPower_min: ' + playPower_min);
+                // console.log('playPower_max: ' + playPower_max);
+
                 var emy_power = $(this).find("div.encounter-power").text();
                 emy_power = emy_power.toLowerCase();
                 emy_power = emy_power.replace('power: ', '');
@@ -179,12 +284,12 @@ function calculatorWinRate() {
                 }
                 $('div#info-win-lose-ext-' + idx + '').html('');  // Remove old calculator Rate
                 var css_rate = 'low-win'
-                if (winRate > 90) {
+                if (winRate >= 90) {
                     css_rate = 'height-win'
                 }
                 $('div#info-win-lose-ext-' + idx + '').append('<span><b>You Pow: </b>' + playPower_min + ' -> ' + playPower_max + '</span>');
                 $('div#info-win-lose-ext-' + idx + '').append('<span><b>Enemy Pow: </b>' + emy_Power_min + ' -> ' + emy_Power_max + '</span>');
-                $('div#info-win-lose-ext-' + idx + '').append('<span><b>Win Rate: </b><el class="' + css_rate + '">' + winRate + '%</el> +<el style="color:#28aafd">' + xp + '</el></span>');
+                $('div#info-win-lose-ext-' + idx + '').append('<span><b>Win Rate: </b><el class="' + css_rate + '">' + winRate + '%</el> <el style="color:#28aafd">' + xp + '</el></span>');
                 if (buni_star != undefined && buni_star > 0) {
                     var rewardGasOffset = 0.5, rewardBaseline = 0.4;
                     var rewardMultiplier = Math.sqrt(emy_power / 1000 * buni_star);
